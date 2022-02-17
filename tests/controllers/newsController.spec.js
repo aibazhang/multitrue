095542f015660e381a435580294417a8c8f4cb56
aibazhang <dtjp@tutanota.com>
2022-02-17 16:58:29 +0900

    mod: newsController.spec.js

--- tests/controllers/newsController.spec.js
+++ tests/controllers/newsController.spec.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 const { mockReq, mockRes } = require('sinon-express-mock');
-// const News = require('../../src/models/newsModel');
 const newsController = require('../../src/controllers/newsController');
 
+let mockError = false;
 jest.mock('../../src/models/newsModel', () => ({
   create: jest.fn((news) => {
+    if (mockError) {
+      return Promise.reject(Error, 'Mock Error');
+    }
     const _id = '12345';
     return Promise.resolve({ _id, ...news });
   }),
@@ -35,5 +38,31 @@ describe('../../src/controller/newsController', () => {
     const news = res.json.getCall(0).args[0].data.newNews;
     delete news._id;
     expect(news).toEqual(testNews);
+  });
+});
+
+describe('Exception', () => {
+  test('mock error', async () => {
+    mockError = true;
+
+    const testNews = {
+      country: 'us',
+      category: 'general',
+      title: 'title1',
+      description: 'description!!',
+      url: 'news.com',
+      publishedAt: Date.now(),
+    };
+
+    const request = {
+      body: testNews,
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+
+    await newsController.createNews(req, res);
+
+    expect(res.status.calledWith(404)).toBeTruthy();
   });
 });
