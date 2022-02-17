8f6f924e28bc507268c16d7d30d3ef7c490a6688
aibazhang <dtjp@tutanota.com>
2022-02-17 13:25:28 +0000

    add: test case for newsModle

--- tests/models/newsModel.spec.js
+++ tests/models/newsModel.spec.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 const News = require('../../src/models/newsModel');
 
-// テストデータ
 const news = [
   {
     country: 'us',
@@ -43,9 +42,26 @@ describe('src/models/newsModel', () => {
     describe('findOrCreate', () => {
       test('get news by specific country and title', async () => {
         const result = await News.findOne({ country: 'us', title: 'title1' });
-        console.log(result);
         expect(result?.title).toEqual('title1');
       });
+      test('get news by specific country and title [Japanese]', async () => {
+        const result = await News.findOne({ country: 'jp', title: '日本語' });
+        expect(result?.title).toEqual('日本語');
+      });
+    });
+  });
+
+  describe('Validation', () => {
+    test('genral and title is multikey indexes', async () => {
+      const invalidNews = {
+        country: 'us',
+        category: 'general',
+        title: 'title1',
+        description: 'description',
+        url: 'news.com',
+        publishedAt: Date.now(),
+      };
+      await expect(News.create(invalidNews)).rejects.toThrow();
     });
   });
 });
