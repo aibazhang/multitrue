7749766b4f8862a11f7b76cce9e712612d8b5024
aibazhang <dtjp@tutanota.com>
2022-03-07 08:45:00 +0000

    add: unit test for appError

--- tests/controllers/authController.spec.js
+++ tests/controllers/authController.spec.js
@@ -7,6 +7,10 @@ jest.mock('../../src/models/userModel', () => ({
     const _id = '12345';
     return Promise.resolve({ _id, ...user });
   }),
+  findOne: jest.fn().mockReturnThis(),
+  select: jest.fn(() =>
+    Promise.resolve({ email: 'example@email.com', password: 'pass1245' })
+  ),
 }));
 
 describe('../../src/controllers/authController', () => {
@@ -19,13 +23,57 @@ describe('../../src/controllers/authController', () => {
     };
   });
 
-  it('should signup', async () => {
+  describe('signup', () => {
+    it('should signup', async () => {
+      const user = {
+        email: 'example@email.com',
+        name: 'name',
+        password: 'pass1234',
+        passwordConfirm: 'pass1234',
+        role: 'admin',
+      };
+
+      const request = {
+        body: user,
+      };
+
+      const req = mockReq(request);
+      const res = mockRes();
+
+      await authController.signup(req, res);
+
+      expect(res.status.calledWith(201)).toBeTruthy();
+
+      const { token } = res.json.getCall(0).args[0];
+      expect(token).toBeTruthy();
+    });
+  });
+
+  describe('login', () => {
+    it('should login', async () => {
+      const user = {
+        email: 'example@email.com',
+        password: 'pass1234',
+      };
+
+      const request = {
+        body: user,
+      };
+
+      const req = mockReq(request);
+      const res = mockRes();
+
+      await authController.login(req, res);
+
+      expect(res.status.calledWith(201)).toBeTruthy();
+
+      const { token } = res.json.getCall(0).args[0];
+      expect(token).toBeTruthy();
+    });
+  });
+  it('should not login without password', async () => {
     const user = {
       email: 'example@email.com',
-      name: 'name',
-      password: 'pass1234',
-      passwordConfirm: 'pass1234',
-      role: 'admin',
     };
 
     const request = {
@@ -35,11 +83,41 @@ describe('../../src/controllers/authController', () => {
     const req = mockReq(request);
     const res = mockRes();
 
-    await authController.signup(req, res);
+    await expect(authController.login(req, res)).rejects.toThrow(
+      /Please provide email and password!/
+    );
+  });
+  it('should not login without email', async () => {
+    const user = {
+      email: 'example@email.com',
+      password: 'pass1234',
+    };
 
-    expect(res.status.calledWith(201)).toBeTruthy();
+    const request = {
+      body: user,
+    };
 
-    const { token } = res.json.getCall(0).args[0];
-    expect(token).toBeTruthy();
+    const req = mockReq(request);
+    const res = mockRes();
+
+    await expect(authController.login(req, res)).rejects.toThrow(
+      /Please provide email and password!/
+    );
+  });
+  it('should not login with uncorrect password', async () => {
+    const user = {
+      password: 'pass1234',
+    };
+
+    const request = {
+      body: user,
+    };
+
+    const req = mockReq(request);
+    const res = mockRes();
+
+    await expect(authController.login(req, res)).rejects.toThrow(
+      /Please provide email and password!/
+    );
   });
 });
