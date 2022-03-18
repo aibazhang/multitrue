66be7ae1b8c18f16eefb4e19e899dda480bb7810
aibazhang <dtjp@tutanota.com>
2022-03-18 06:38:23 +0000

    add: add periodical http get to wake up heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable comma-dangle */
+const http = require('http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./src/app');
@@ -28,6 +29,13 @@ const server = app.listen(port, () => {
     console.log(
       `Import data job running... Next run on ${importDataJob.next()}`
     );
+  }
+
+  // Wake up Heroku
+  if (process.env.NODE_ENV === 'production') {
+    setInterval(() => {
+      http.get('https://multitrue.herokuapp.com/');
+    }, 15 * 60 * 1000);
   }
 });
 
